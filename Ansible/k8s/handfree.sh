9cf478fc81a321666b218d5af33a6a902387bed2
venusnvenu <venus.vmware@gmail.com>
2024-08-06 12:49:06 +0530

    absolute version v1.30

--- Ansible/k8s/handfree.sh
+++ Ansible/k8s/handfree.sh
@@ -2,3 +2,4 @@ ansible-playbook 1.pre-steps.yml -i inventory.yml --private-key ~/.ssh/id_rsa -u
 ansible-playbook 2.kubernetes-setup.yml -i inventory.yml --private-key ~/.ssh/id_rsa -u venu  -K
 ansible-playbook 3.master-initiate.yml -i inventory.yml --private-key ~/.ssh/id_rsa -u venu  -K
 ansible-playbook 4.woker-k8s-join.yml -i inventory.yml --private-key ~/.ssh/id_rsa -u venu  -K
+ansible-playbook 5.cni-calio.yml -i inventory.yml --private-key ~/.ssh/id_rsa -u venu  -K
