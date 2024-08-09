bad913b3a7c95d4dd16a70ac16f7d4461ad97929
venusnvenu <venus.vmware@gmail.com>
2024-08-09 18:42:43 +0530

    added yaml to creat users , groups and add users to groups

--- Ansible/k8s/handfree.sh
+++ Ansible/k8s/handfree.sh
@@ -1,5 +1,5 @@
-ansible-playbook 1.pre-steps.yml -i inventory.yml --private-key ~/.ssh/id_rsa -u venu  -K
-ansible-playbook 2.kubernetes-setup.yml -i inventory.yml --private-key ~/.ssh/id_rsa -u venu  -K
-ansible-playbook 3.master-initiate.yml -i inventory.yml --private-key ~/.ssh/id_rsa -u venu  -K
-ansible-playbook 4.woker-k8s-join.yml -i inventory.yml --private-key ~/.ssh/id_rsa -u venu  -K
-ansible-playbook 5.cni-calio.yml -i inventory.yml --private-key ~/.ssh/id_rsa -u venu  -K
+ansible-playbook 1.pre-steps.yml -i inventory.yml --private-key ~/.ssh/id_rsa -K
+ansible-playbook 2.kubernetes-setup.yml -i inventory.yml --private-key ~/.ssh/id_rsa  -K
+ansible-playbook 3.master-initiate.yml -i inventory.yml --private-key ~/.ssh/id_rsa  -K
+ansible-playbook 4.woker-k8s-join.yml -i inventory.yml --private-key ~/.ssh/id_rsa  -K
+ansible-playbook 5.cni-calio.yml -i inventory.yml --private-key ~/.ssh/id_rsa  -K
